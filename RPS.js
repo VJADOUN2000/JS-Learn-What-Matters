04fc60f70c06c2ca37daaf89410dea3049075673
VJADOUN2000 <jadounvikrant@gmail.com>
2026-01-02 21:50:47 +0530

    sample (for loop)

--- RPS.js
+++ RPS.js
@@ -10,6 +10,18 @@ function rps(user,computer){
    return "Computer Wins"
 }
 
-let player = prompt("Enter the your gesture [Player 1]");
-let player2 = prompt("Enter the your gesture  [Player 2]");
-console.log(rps(player,player2));
+//let player = prompt("Enter the your gesture [Player 1]");
+//let player2 = prompt("Enter the your gesture  [Player 2]");
+//console.log(rps(player,player2));
+
+
+//-------------------------Loops----------------------------------->
+
+//repear karne ko loop kahte hai
+
+//for loop
+
+let num =10;
+for(let i=0;i<num;i++){
+    console.log(i);
+}
